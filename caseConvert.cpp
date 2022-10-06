d8cdecf59f429818beb3229e767aed82850b8032
Raiyan Aaijaz <raiyanjaz@gmail.com>
2022-10-06 17:10:50 -0400

    Create caseConvert.cpp

---
+++ caseConvert.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main () {
+    char characterInput;
+    
+    cout << "Please input a character:";
+    cin >> characterInput;
+    cout << "Changes";
+    cout << "change";
+}
