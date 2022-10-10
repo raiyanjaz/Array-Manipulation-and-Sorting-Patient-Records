8f763face28c7694889f4cdda5c89be0d311c230
Raiyan Aaijaz <raiyanjaz@gmail.com>
2022-10-10 00:11:06 -0400

    Updated README and completed caseConversion

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# Assignment 2a
- 
+# Assignment 2
+
+caseConversion.cpp instructions:
+    Write a program that takes a character from terminal as input, and provide one of the following three outputs depending on the input character:
+        1. Input is a lower-case letter from the English alphabet: The program should output the    corresponding upper-case letter.
+        2. Input is an upper-case letter from the English alphabet: The program should output the corresponding lower-case letter.
+        3. Input is anything other than an upper or lower-case letter from the English alphabet: The program should echo the input character, i.e., output exactly the character that was input
--- caseConversion.cpp
+++ caseConversion.cpp
@@ -1,16 +1,26 @@
-#include <iostream>
-#include <string>
+#include <iostream> // Include I/O library
 
-using namespace std;
+using namespace std; // The standard namespace was declared to define the I/O object "cout"
 
-int main () {
-    char arr[100];
-    char characterInput;
+int main () { // Main function
+    char characterInput; // Declaring the char variable characterInput
     
-    cout << "Please input a character: ";
-    cin >> characterInput;
+    cout << "Please input a character: "; //Asking the user to input a character
+    cin >> characterInput; // Allows user to input a character to initialize characterInput
     
-    int number = characterInput - 32;
-    char uppercase = number;
-    cout << "The characterInput changes to " << uppercase << endl;
+    // The characterInput is converted into an integer based on the ASCII Table
+    int characterInputNumber = characterInput;
+
+    if (characterInputNumber >= 97 && characterInputNumber <= 122) { // If statement that will run following code when character input number is between 97 and 122 (checks if characterInput is lowercase)
+        int uppercaseNumber = characterInputNumber - 32; // Subtracts 32 from character input number to change the lowercase character into its uppercase
+        char uppercase = uppercaseNumber; // Converts the uppercase number into its corresponding uppercase value based on the ASCII Table
+        cout << "The uppercase of " << characterInput << " is " << uppercase << endl; // Tells the user the uppercase character of the lowercase character they had input
+    } else if (characterInputNumber >= 65  && characterInputNumber <= 90) { // Else If statement that will run following code when character input number is between 65 and 90 (checks if characterInput is uppercase)
+        int lowercaseNumber = characterInputNumber + 32; // Adds 32 to the characterInputNumber to change the uppercase character into its lowercase
+        char lowercase = lowercaseNumber; // Converts the lowercase number into its corresponding lowercase value based on the ASCII Table
+        cout << "The lowercase of " << characterInput << " is " << lowercase << endl; // Tells the user the lowercase character of the uppercase character they had input
+    } else if (characterInputNumber > 122 || characterInputNumber < 65 || characterInputNumber >=91 && characterInputNumber <= 96) { 
+        // Else If statement that will run following code (checks if the character they input is not an uppercase or lowercase character)
+        cout << "The input character is " << characterInput << endl; // Repeats to the user the character they had input    
+    }
 }
