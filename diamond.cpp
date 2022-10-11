e6c89146ff0e961b4f0ead63972e0d1457338dcd
Raiyan Aaijaz <raiyanjaz@gmail.com>
2022-10-11 13:55:13 -0400

    created half of top half of diamond.cpp

--- diamond.cpp
+++ diamond.cpp
@@ -8,15 +8,34 @@ int main () {
     cout << "How large do you want the diamond to be: ";
     cin >> diamondSize;
 
-    for (int i = 1; i <= (diamondSize-1); i++) 
+    // Top half of diamond
+
+    for (int i = 1; i <= (diamondSize-1); i++) // For loop to create the first line in the diamond shape
         cout << " ";  
     cout << "/\\" << endl;
 
-    for (int i = 2; i <= (diamondSize-1); i++)
+    for (int x = 2; x <= (diamondSize-1); x++) {  // For loop to create the middle lines in the diamond shape
+        
+        for (int i = 1; i <= (diamondSize-x); i++) 
+            cout << " ";
+        cout << "/"; 
+
+        for (int i = 2; i <= (diamondSize-x); i++)
+            cout << " ";
+        cout << "\\" << endl;
+    }
+
+    for (int i = 1; i >= (diamondSize-1); i++) // For loop to create the middle line or the last line in the top half
         cout << " ";
     cout << "/" << endl;
 
-    for (int i = (diamondSize-1); i <= (diamondSize-1); i++)
+    // Bottom half of diamond
+    
+    for (int i = 1; i >= (diamondSize-1); i++)
         cout << " ";
-    cout << "/" << endl;   
+    cout << "\\" << endl;
+    
+    for (int x = 4; x <= (diamondSize-1) && x>0; x--)
+        cout << " ";
+    cout << "\\" << endl;
 }
