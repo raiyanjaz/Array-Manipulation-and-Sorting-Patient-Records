af161effe04ea275d04bd7fed9060c61cf0c520a
Raiyan Aaijaz <raiyanjaz@gmail.com>
2022-10-14 00:16:30 -0400

    Reversed the order of char array

--- largeSum.cpp
+++ largeSum.cpp
@@ -24,21 +24,18 @@ int main (const int argc, const char* const argv[]) { // Main function with comm
 
     char finalNumber[x];
 
-    for (int i = length1 - 1; i >= 0; i--) { // Stores the values from argv1 into char array number1
-        number1[i] = argv[1][i];
+    for (int i = length1 - 1; i >= 0; i--) { // Stores the values from argv1 into char array number1 and reverses the order
+        number1[(length1 - 1) - i] = argv[1][i]; 
     }
 
-    for (int i = length2 - 1; i >= 0; i--) // Stores the values from argv2 into char array number2
-        number2[i] = argv[2][i];
-
-    reverse(number1.begin[length1], number1.end[length1]);
-
-    
+    for (int i = length2 - 1; i >= 0; i--) // Stores the values from argv2 into char array number2 and reverses the order
+        number2[(length2 - 1) - i] = argv[2][i];
     
     for (int i = 0; i < length1; i++) {
-        int sum = (number1[i] - '0') + (number2[i] - '0') + carry
-
+        int sum = (number1[i] - '0') + (number2[i] - '0') + carry;
     }
+
+
     
     /*
     for (int i = strlen(argv[1]) - 1; i >= 0; i--) { 
@@ -56,5 +53,9 @@ int main (const int argc, const char* const argv[]) { // Main function with comm
 
     for (int i = 0; i <= strlen(argv[1]) - 1; i++)
         cout << number1[i];
+    
+    cout << "" << endl;
 
+    for (int i = 0; i <= strlen(argv[1]) - 1; i++)
+        cout << number2[i];
 }
